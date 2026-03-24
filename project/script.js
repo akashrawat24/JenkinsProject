def32f6f678726628b5d025b8aba829629a7cbb8
akashrawat24 <96379709+akashrawat24@users.noreply.github.com>
2026-03-24 14:05:47 +0530

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,5 +1,5 @@
-document.getElementById("form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    document.getElementById("output").innerText = "Hello " + name;
-});
+function scrollToSection() {
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+}
